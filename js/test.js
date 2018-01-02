02fb36b550fed7cefc23c39971a336e89b12770d
aaleexom <alejandroolivaresmompo@hotmail.com>
2018-01-02 15:41:35 +0100

    Add files via upload

--- js/test.js
+++ js/test.js
@@ -95,11 +95,22 @@ function roll(){
 }
 
 function autoRoll(){
-	var delay = $("#msDelay").val();
+	var pocketBTC = $("#btcCounter").text();
+	var pocketBTCFix = parseFloat(pocketBTC).toFixed(8);
+	var betAmountCounter = $("#betAmount").val();
+	var betAmountCounterFix = parseFloat(betAmountCounter);
 
-	if (startedAutoRoll == true){
-		roll();
-		setTimeout(autoRoll, delay);
+	if ((pocketBTCFix > 0.00000000) && (pocketBTCFix >= betAmountCounterFix)){
+		var delay = $("#msDelay").val();
+
+		if (startedAutoRoll == true){
+			roll();
+			setTimeout(autoRoll, delay);
+		}
+	}
+	else {
+		$("#msgTrueFalse").html("<p class=\"py-1\">No tienes suficiente dinero en el monedero.</p>");
+		switchAuto();
 	}
 }
 
@@ -146,69 +157,89 @@ $(document).ready(function(){
 	$("#autoRollBody").hide();
 
 	$("#btnHi").click(function(){
-		var x = Math.floor(Math.random() * 10000);
-		var trueOrFalse = true;
-		var betVar = "Hi";
-		trueOrFalse = checkHi(x);
-		var xLength = x.toString().length;
-		if(xLength == 1){
-			x = "0000" + x;
-		}
-		else if (xLength == 2){
-			x = "000" + x;
-		}
-		else if (xLength == 3){
-			x = "00" + x;
-		}
-		else if (xLength == 4){
-			x = "0" + x;
-		}
-		$("#rndNumber").text(x);
+		var pocketBTC = $("#btcCounter").text();
+		var pocketBTCFix = parseFloat(pocketBTC).toFixed(8);
+		var betAmountCounter = $("#betAmount").val();
+		var betAmountCounterFix = parseFloat(betAmountCounter);
 
-		if (trueOrFalse == true) {
-			$("#msgTrueFalse").html("<p class=\"bg-success py-1\">¡Has ganado!</p>");
-			var text = "<tr><td scope=\"row\" class=\"bg-dark text-white\" id=\"bet\">" + betVar + "</td><td class=\"roll\">" + x + "</td><td class=\"result\"><b class=\"text-success\">Win</b></td></tr>";
-			$("#btcCounter").text(addCoins);
+		if ((pocketBTCFix > 0.00000000) && (pocketBTCFix >= betAmountCounterFix)){
+			var x = Math.floor(Math.random() * 10000);
+			var trueOrFalse = true;
+			var betVar = "Hi";
+			trueOrFalse = checkHi(x);
+			var xLength = x.toString().length;
+			if(xLength == 1){
+				x = "0000" + x;
+			}
+			else if (xLength == 2){
+				x = "000" + x;
+			}
+			else if (xLength == 3){
+				x = "00" + x;
+			}
+			else if (xLength == 4){
+				x = "0" + x;
+			}
+			$("#rndNumber").text(x);
+
+			if (trueOrFalse == true) {
+				$("#msgTrueFalse").html("<p class=\"bg-success py-1\">¡Has ganado!</p>");
+				var text = "<tr><td scope=\"row\" class=\"bg-dark text-white\" id=\"bet\">" + betVar + "</td><td class=\"roll\">" + x + "</td><td class=\"result\"><b class=\"text-success\">Win</b></td></tr>";
+				$("#btcCounter").text(addCoins);
+			}
+			else {
+				$("#msgTrueFalse").html("<p class=\"bg-danger py-1\">Has perdido...</p>");
+				var text = "<tr><td scope=\"row\" class=\"bg-dark text-white\" id=\"bet\">" + betVar + "</td><td class=\"roll\">" + x + "</td><td class=\"result\"><b class=\"text-danger\">Lose</b></td></tr>";
+				$("#btcCounter").text(removeCoins);
+			}
+			$("#tbody").prepend(text);
 		}
 		else {
-			$("#msgTrueFalse").html("<p class=\"bg-danger py-1\">Has perdido...</p>");
-			var text = "<tr><td scope=\"row\" class=\"bg-dark text-white\" id=\"bet\">" + betVar + "</td><td class=\"roll\">" + x + "</td><td class=\"result\"><b class=\"text-danger\">Lose</b></td></tr>";
-			$("#btcCounter").text(removeCoins);
+			$("#msgTrueFalse").html("<p class=\"py-1\">No tienes suficiente dinero en el monedero.</p>");
 		}
-		$("#tbody").prepend(text);
 	});
 
 	$("#btnLo").click(function(){
-		var x = Math.floor(Math.random() * 10000);
-		var trueOrFalse = true;
-		var betVar = "Lo";
-		trueOrFalse = checkLo(x);
-		var xLength = x.toString().length;
-		if(xLength == 1){
-			x = "0000" + x;
-		}
-		else if (xLength == 2){
-			x = "000" + x;
-		}
-		else if (xLength == 3){
-			x = "00" + x;
-		}
-		else if (xLength == 4){
-			x = "0" + x;
-		}
-		$("#rndNumber").text(x);
+		var pocketBTC = $("#btcCounter").text();
+		var pocketBTCFix = parseFloat(pocketBTC).toFixed(8);
+		var betAmountCounter = $("#betAmount").val();
+		var betAmountCounterFix = parseFloat(betAmountCounter);
 
-		if (trueOrFalse == true) {
-			$("#msgTrueFalse").html("<p class=\"bg-success py-1\">¡Has ganado!</p>");
-			var text = "<tr><td scope=\"row\" class=\"bg-dark text-white\" id=\"bet\">" + betVar + "</td><td class=\"roll\">" + x + "</td><td class=\"result\"><b class=\"text-success\">Win</b></td></tr>";
-			$("#btcCounter").text(addCoins);
+		if ((pocketBTCFix > 0.00000000) && (pocketBTCFix >= betAmountCounterFix)){
+			var x = Math.floor(Math.random() * 10000);
+			var trueOrFalse = true;
+			var betVar = "Lo";
+			trueOrFalse = checkLo(x);
+			var xLength = x.toString().length;
+			if(xLength == 1){
+				x = "0000" + x;
+			}
+			else if (xLength == 2){
+				x = "000" + x;
+			}
+			else if (xLength == 3){
+				x = "00" + x;
+			}
+			else if (xLength == 4){
+				x = "0" + x;
+			}
+			$("#rndNumber").text(x);
+
+			if (trueOrFalse == true) {
+				$("#msgTrueFalse").html("<p class=\"bg-success py-1\">¡Has ganado!</p>");
+				var text = "<tr><td scope=\"row\" class=\"bg-dark text-white\" id=\"bet\">" + betVar + "</td><td class=\"roll\">" + x + "</td><td class=\"result\"><b class=\"text-success\">Win</b></td></tr>";
+				$("#btcCounter").text(addCoins);
+			}
+			else {
+				$("#msgTrueFalse").html("<p class=\"bg-danger py-1\">Has perdido...</p>");
+				var text = "<tr><td scope=\"row\" class=\"bg-dark text-white\" id=\"bet\">" + betVar + "</td><td class=\"roll\">" + x + "</td><td class=\"result\"><b class=\"text-danger\">Lose</b></td></tr>";
+				$("#btcCounter").text(removeCoins);
+			}
+			$("#tbody").prepend(text);
 		}
 		else {
-			$("#msgTrueFalse").html("<p class=\"bg-danger py-1\">Has perdido...</p>");
-			var text = "<tr><td scope=\"row\" class=\"bg-dark text-white\" id=\"bet\">" + betVar + "</td><td class=\"roll\">" + x + "</td><td class=\"result\"><b class=\"text-danger\">Lose</b></td></tr>";
-			$("#btcCounter").text(removeCoins);
+			$("#msgTrueFalse").html("<p class=\"py-1\">No tienes suficiente dinero en el monedero.</p>");
 		}
-		$("#tbody").prepend(text);
 	});
 
 	$("#autoRoll").click(function(){
